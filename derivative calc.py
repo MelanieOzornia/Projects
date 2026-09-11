e151933c7f1e0b7a3e89e75a3e7434df8fe35c19
MelanieOzornia <melanieozornia@gmail.com>
2026-09-10 23:53:29 -0400

    first commit calc

--- derivative calc.py
+++ derivative calc.py
@@ -1,4 +1,4 @@
 co = input("what is the coefficient of x?\n")
 ex = input("what is the exponent of x?\n")
-
+#rints result of derivative calculation
 print("The derivative of ", co, "x^", ex, " is: ", int(co) * int(ex), "x^", int(ex) - 1)
