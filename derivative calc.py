5a462715e2e0e552c63a81ab2406d54de10c7a7d
MelanieOzornia <melanieozornia@gmail.com>
2026-09-11 23:12:58 -0400

    update added integrals

--- derivative calc.py
+++ derivative calc.py
@@ -1,4 +1,15 @@
+from fractions import Fraction
+
+function = input("Do you want to differentiate or integrate?\nplease type 'd' for differentiate or 'i' for integrate\n")
+
+
 co = input("what is the coefficient of x?\n")
 ex = input("what is the exponent of x?\n")
-#rints result of derivative calculation
-print("The derivative of ", co, "x^", ex, " is: ", int(co) * int(ex), "x^", int(ex) - 1)
+
+# differentiation calculation
+if function == "d":
+    #prints result of derivative calculation
+    print("The derivative of ", co, "x^", ex, " is: ", int(co) * int(ex), "x^", int(ex) - 1)
+else:
+    # integration calculation
+    print("The integral of ", co, "x^", ex, " is: ", Fraction(int(co), int(ex) + 1), "x^", int(ex) + 1)
